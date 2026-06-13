6c89353f66e2e593ef05640f311b881d8d4eb8c4
SujalPawar <pawarsujal4455@gmail.com>
2026-06-13 22:04:48 +0530

    Added Reverse Array Code

--- ArrayI.java
+++ ArrayI.java
@@ -40,8 +40,30 @@ public class ArrayI {
                 end=mid-1
             }
         }
-    }
-    if(!found){
+         if(!found){
         System.out.println(key+" Not Found");
+        }
+
     }
+
+    public static void reverseArray(){
+        int arr[]={1,2,3,4,5,6};
+        int left=0;
+        int right=arr.length-1;
+
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.println("Reversed Array:")
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+   
+
 }
