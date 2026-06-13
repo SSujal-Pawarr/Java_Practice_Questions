fa93eaf7918dc65bec70285d9eee81973a5f2ffc
SujalPawar <pawarsujal4455@gmail.com>
2026-06-13 22:09:23 +0530

    Added Pairs in an array code

--- ArrayI.java
+++ ArrayI.java
@@ -64,6 +64,18 @@ public class ArrayI {
         }
     }
 
+    public static void pairsInAnArray(){
+        int arr[]={1,2,3,4,5,6};
+
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("(" + curr + "," + arr[j] + ") ");
+            }
+            System.out.println();
+        }
+    }
+
    
 
 }
