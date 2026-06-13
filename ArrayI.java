2d2415c0d434f3d51286693a0948703324afadb7
SujalPawar <pawarsujal4455@gmail.com>
2026-06-13 22:01:10 +0530

    Added BinarySearch Code

--- ArrayI.java
+++ ArrayI.java
@@ -21,4 +21,27 @@ public class ArrayI {
             System.out.println(value + " Not Found");
         }
     }
+
+    public static void binarySearch(){
+        int arr[] = {1,2,3,4,5,6};
+        int key=5;
+        int start=0;
+        int end=arr.length-1;
+        Boolean found=false;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==key){
+                System.out.println(key+" Found");
+                found=true
+                break;
+            }else if (arr[mid]<key){
+                start=mid+1;
+            }else{
+                end=mid-1
+            }
+        }
+    }
+    if(!found){
+        System.out.println(key+" Not Found");
+    }
 }
