c799ee04a752d306573737a1eecc576bb584871c
SujalPawar <pawarsujal4455@gmail.com>
2026-06-21 14:13:17 +0530

    added maxSubArraySumII code

--- ArrayI.java
+++ ArrayI.java
@@ -108,36 +108,6 @@ public class ArrayI {
 
 
 
-    public static void maxSubArraySumII() {
-
-        int arr[] = {1,2,3,4,5};
-        int maxSum = Integer.MIN_VALUE;
-
-        int prefix[] = new int[arr.length];
-        prefix[0] = arr[0];
-
-        for(int i=1;i<arr.length;i++){
-             prefix[i] = prefix[i-1] + arr[i];
-        }
-
-        for(int i=0;i<arr.length;i++){
-            int start = i;
-
-            for(int j=i;j<arr.length;j++){
-                int end = j;
-
-                int currSum = (start == 0)
-                    ? prefix[end]
-                    : prefix[end] - prefix[start-1];
-
-                if(currSum > maxSum){
-                    maxSum = currSum;
-                }
-            }
-        }
-
-     System.out.println(maxSum);
-    }
 
     public static void kadanesAlgorithm(){
         int prices[]={7,1,5,3,6,4};
