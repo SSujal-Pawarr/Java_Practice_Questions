dc59313e21b427eed627caa9a6b265f3f63e8f35
SujalPawar <pawarsujal4455@gmail.com>
2026-06-21 14:18:52 +0530

    modify ArrayII.java file

--- ArrayII.java
+++ ArrayII.java
@@ -60,7 +60,7 @@ public class ArrayII{
         int prices[]={7,1,5,3,6,4};
         int buyPrice=Integer.MAX_VALUE;
         int profit=0;
-int maxProfit=0;
+        int maxProfit=0;
         for(int i=0;i<prices.length;i++){
             if(buyPrice<prices[i]){
                 //profit
