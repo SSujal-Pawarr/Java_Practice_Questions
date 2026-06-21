9d106f7bd1faca8bf03a3f5cefe3511ba71b3af1
SujalPawar <pawarsujal4455@gmail.com>
2026-06-21 14:15:21 +0530

    added trappedRainwater code  code

--- ArrayII.java
+++ ArrayII.java
@@ -56,7 +56,7 @@ public class ArrayII{
      System.out.println(maxSum);
     }
 
-     public static void kadanesAlgorithm(){
+    public static void kadanesAlgorithm(){
         int prices[]={7,1,5,3,6,4};
         int buyPrice=Integer.MAX_VALUE;
         int profit=0;
@@ -74,4 +74,32 @@ public class ArrayII{
 
     }
 
+    public static void trappedRainWater(){
+        int height[]={4,2,0,6,3,2,5};
+        int n=height.length;
+
+        int leftMax[]=new int[n];
+        leftMax[0]=height[0];
+
+        for(int i=1;i<n;i++){
+            leftMax[i]=Math.max(leftMax[i-1],height[i]);
+        }
+
+        int rightMax[]=new int[n];
+        rightMax[n-1]=height[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=Math.max(rightMax[i+1],height[i]);
+        }
+
+        int trappedWater=0;
+        for(int i=0;i<n;i++){
+            int waterLevel=Math.min(leftMax[i],rightMax[i]);
+
+            trappedWater+=waterLevel-height[i];
+        }
+
+        System.out.println(trappedWater);
+
+    }
 }
