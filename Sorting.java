4ea5c9bee23526752e8922b3744193605111def9
SujalPawar <pawarsujal4455@gmail.com>
2026-06-24 18:53:57 +0530

    Added bubbleSort COdee

---
+++ Sorting.java
@@ -0,0 +1,21 @@
+public class Sorting{
+    public static void main(String []args){
+        bubbleSort();
+    }
+    public static void bubbleSort(){
+        int arr[]={5,1,3,6};
+
+        for(int turn=0;turn<arr.length-1;turn++){
+            for(int j=0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
